f2767d1b545a23d4e1c56e8715702198db336d3f
Billy Eclipse <Billy Eclipse>
2018-04-20 03:37:35 +0300

    Lectures 297-301

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	private Vector3 inputValue;
+	
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+		transform.Translate(inputValue);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponentInChildren<Camera>().enabled = true;
+	}
+}
